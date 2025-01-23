b22ddd08d3c7972a46109912585ac34ca39788e6
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-23 01:55:25 -0500

    test(Ship): add testing for Ship class

---
+++ src/test/ship.test.js
@@ -0,0 +1,22 @@
+import { expect, describe, it } from '@jest/globals';
+import { Ship } from '../gameLogic/ship';
+
+describe('Ship Class', () => {
+  const ship = new Ship();
+
+  it('Should be an object', () => {
+    expect(ship).toBeInstanceOf(Object);
+  });
+
+  it('Should include ship length', () => {
+    expect(ship).toHaveProperty('length');
+  });
+
+  it('Should contain number of "hits"', () => {
+    expect(ship).toHaveProperty('hits');
+  });
+
+  it('Should tell you if a ship has sunk', () => {
+    expect(ship).toHaveProperty('sunk');
+  });
+});
